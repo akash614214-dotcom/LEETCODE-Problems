b99f95e51cfa50ba2c1b2c252d4a059a41c82f44
Akash sharma <akash614214@gmail.com>
2026-07-23 14:30:47 +0530

    Time: 0 ms (100%), Space: 8.5 MB (94.95%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int lengthOfLastWord(char *s) {
+    int len = 0;
+    int i = strlen(s) - 1;
+
+    while (i >= 0 && s[i] == ' ') {
+        i--;
+    }
+
+    while (i >= 0 && s[i] != ' ') {
+        len++;
+        i--;
+    }
+
+    return len;
+}
