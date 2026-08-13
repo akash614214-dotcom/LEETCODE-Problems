9ac0de4b3787aff750eb4cae8fc80541610dcf69
Akash sharma <akash614214@gmail.com>
2026-08-13 21:18:05 +0530

    Time: 104 ms (5.01%), Space: 9.7 MB (45.6%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.c
+++ 0045-jump-game-ii/0045-jump-game-ii.c
@@ -1,27 +1,16 @@
 int jump(int* nums, int n) {
     int dp[n];
 
-    for (int i = 0; i < n; i++)
-        dp[i] = 1000000;
-
     dp[0] = 0;
+
+    for (int i = 1; i < n; i++)
+        dp[i] = 1000000;
 
     for (int i = 0; i < n; i++) {
         for (int j = 1; j <= nums[i] && i + j < n; j++) {
-            int x = dp[i] + 1;
-
-            if (x < dp[i + j])
-                dp[i + j] = x;
+            if (dp[i] + 1 < dp[i + j])
+                dp[i + j] = dp[i] + 1;
         }
-    }
-
-    for (int i = 0; i < n; i++) {
-        int x = dp[i];
-
-        for (int j = 0; j < 10; j++)
-            x = x;
-
-        dp[i] = x;
     }
 
     return dp[n - 1];
