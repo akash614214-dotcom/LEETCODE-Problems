a9327afa506cad7ee6cd1cde9f0b74a4ee758e5a
Akash sharma <akash614214@gmail.com>
2026-09-04 09:33:18 +0530

    Time: 0 ms (100%), Space: 10.3 MB (94.18%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.c
@@ -0,0 +1,54 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#include <stdlib.h>
+
+int findFirst(int* nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize - 1;
+    int index = -1;
+    
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] >= target) {
+            if (nums[mid] == target) {
+                index = mid;
+            }
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    
+    return index;
+}
+
+int findLast(int* nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize - 1;
+    int index = -1;
+    
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] <= target) {
+            if (nums[mid] == target) {
+                index = mid;
+            }
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    return index;
+}
+
+int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
+    int* result = (int*)malloc(2 * sizeof(int));
+    *returnSize = 2;
+    
+    result[0] = findFirst(nums, numsSize, target);
+    result[1] = findLast(nums, numsSize, target);
+    
+    return result;
+}
